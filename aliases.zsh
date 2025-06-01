ac1c438e3ebc232fdf6a429bc54be0718d5dca51
volvicmarz <callofduty2006turk@gmail.com>
2025-06-01 19:55:15 +0200

    hetelem petelem

---
+++ aliases.zsh
@@ -0,0 +1,8 @@
+alias zsh='nvim ~/.zshrc'
+alias hyprland='nvim .config/hypr/hyprland.conf'
+alias anime='ani-cli'
+alias    yayfind='yay -Slq | fzf --multi --preview "$aurhelper -Si {1}" | xargs -ro $aurhelper -S'
+4() {blueman-manager}
+alias update='yay -Syu'
+alias now='shutdown now'
+alias matrix='neo'
